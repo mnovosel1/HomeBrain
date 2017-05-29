a207b9215ae86600f87e670be9747b4cc71a4489
Marijo Novosel <marijo.novosel@gmail.com>
2017-05-29 17:01:39 +0200

    HomeBrain.sh test

--- HomeBrain.sh
+++ HomeBrain.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+case "$1" in
+
+1)  echo "Sending SIGHUP signal"
+    kill -SIGHUP $2
+    ;;
+2)  echo  "Sending SIGINT signal"
+    kill -SIGINT $2
+    ;;
+3)  echo  "Sending SIGQUIT signal"
+    kill -SIGQUIT $2
+    ;;
+9) echo  "Sending SIGKILL signal"
+   kill -SIGKILL $2
+   ;;
+*) echo "Signal number $1 is not processed"
+   ;;
+esac
