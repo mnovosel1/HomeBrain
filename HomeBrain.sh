8bfcb84f0e12c05dfbe127da93900e312a1107f4
Marijo Novosel <marijo.novosel@gmail.com>
2017-05-29 17:17:06 +0200

    HomeBrain.sh tested

--- HomeBrain.sh
+++ HomeBrain.sh
@@ -2,8 +2,9 @@
 
 case "$1" in
 
-"Testing")  echo "Sending TESTING signal"
-   ;;
-*) echo "Signal number $1 is not processed"
-   ;;
+	"Testing")  echo "Sending TESTING signal"
+	   ;;
+
+	*) eval $1
+	   ;;
 esac
