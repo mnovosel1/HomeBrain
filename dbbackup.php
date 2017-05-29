e2b0fd28b562d55ba281e8947f117acbe836dd7d
Marijo Novosel <marijo.novosel@gmail.com>
2017-05-29 12:58:05 +0200

    dbbackup.php update

--- dbbackup.php
+++ dbbackup.php
@@ -10,10 +10,10 @@ $configglobss = parse_ini_file($path .'/configglobs.ini');
 $sql = "
     BEGIN TRANSACTION;
 
-    CREATE TABLE states (
-    name varchar(75) NOT NULL,
-    active int(1) NOT NULL DEFAULT 0
-    );
+        CREATE TABLE states (
+            name varchar(75) NOT NULL,
+            active int(1) NOT NULL DEFAULT 0
+        );
 
 ";
 
@@ -23,34 +23,33 @@ foreach ( $output as $line )
   $sql .= "    ".$line . "\n";
 
 $sql .= "
-    CREATE TABLE changelog (
-    timestamp DATETIME,
-    statebefore varchar(30) NOT NULL,
-    stateid int(11) NOT NULL,
-    changedto int(1) NOT NULL,
-    weight int(11) NOT NULL DEFAULT 1,
-    PRIMARY KEY(statebefore, stateid, changedto),
-    FOREIGN KEY (stateid) REFERENCES states(rowid)
-    );
+        CREATE TABLE changelog (
+            timestamp DATETIME,
+            statebefore varchar(30) NOT NULL,
+            stateid int(11) NOT NULL,
+            changedto int(1) NOT NULL,
+            weight int(11) NOT NULL DEFAULT 1,
+            PRIMARY KEY(statebefore, stateid, changedto),
+            FOREIGN KEY (stateid) REFERENCES states(rowid)
+        );
 ";
 
 $sql .= "
-    CREATE TRIGGER changelog_trigg
-    BEFORE UPDATE
-    ON states
-    FOR EACH ROW
-    WHEN OLD.active <> NEW.active
-    BEGIN
-    INSERT OR REPLACE INTO changelog (timestamp, statebefore, stateid, changedto, weight)
-    VALUES (  datetime('now','localtime'), 
-                (SELECT group_concat(active, '') FROM states ORDER BY rowid ASC), 
-                NEW.rowid, 
-                NEW.active, 
-                (SELECT weight+1 FROM changelog WHERE stateid=NEW.rowid AND changedto=NEW.active)
-            );
-    DELETE FROM changelog WHERE timestamp <= date('now', '-30 day');
-    END;
-    
+        CREATE TRIGGER changelog_trigg
+            BEFORE UPDATE ON states
+            FOR EACH ROW
+            WHEN OLD.active <> NEW.active
+            BEGIN
+                INSERT OR REPLACE INTO changelog (timestamp, statebefore, stateid, changedto, weight)
+                VALUES (  datetime('now','localtime'), 
+                            (SELECT group_concat(active, '') FROM states ORDER BY rowid ASC), 
+                            NEW.rowid, 
+                            NEW.active, 
+                            (SELECT weight+1 FROM changelog WHERE stateid=NEW.rowid AND changedto=NEW.active)
+                        );
+                DELETE FROM changelog WHERE timestamp <= date('now', '-30 day');
+            END;
+
 ";
 
 $output = '';
@@ -58,7 +57,8 @@ exec('sqlite3 '. $path .'/var/hbrain.db \'.dump "changelog"\' | grep \'^INSERT\'
 foreach ( $output as $line )
   $sql .= "    ".$line . "\n";
 
-$sql .= 'COMMIT;';
+$sql .= '    COMMIT;
+';
 
 file_put_contents($path .'/hbrain.sql', $sql);
 /////////////////////////////////////////////////////////////////////////////////////////////
