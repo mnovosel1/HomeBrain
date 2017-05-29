65d048ead1cf23fe1759ace1183de112a1b2318a
Marijo Novosel <marijo.novosel@gmail.com>
2017-05-29 18:10:41 +0200

    wakeCheck.sh update

--- lan/wakeCheck.sh
+++ lan/wakeCheck.sh
@@ -36,14 +36,26 @@ sqlite3 $DIR/var/hbrain.db "UPDATE states SET active=$serverlive WHERE name='Hom
 kodilive=$(ping -c1 10.10.10.10 | grep 'received' | awk -F ',' '{print $2}' | awk '{ print $1}');
 sqlite3 $DIR/var/hbrain.db "UPDATE states SET active=$kodilive WHERE name='KODI'";
 
-usersonhbrain=$(who | wc -l);
-sqlite3 $DIR/var/hbrain.db "UPDATE states SET active=$usersonhbrain WHERE name='HomeBrain user'";
+if [ $(who | wc -l) -gt 0 ] then;
+  active=1
+else
+  active=0
+fi
+sqlite3 $DIR/var/hbrain.db "UPDATE states SET active=$active WHERE name='HomeBrain user'";
 
-usersonhserver=$(/usr/bin/ssh 10.10.10.100 "who | wc -l");
-sqlite3 $DIR/var/hbrain.db "UPDATE states SET active=$usersonhserver WHERE name='HomeServer user'";
+if [ $(/usr/bin/ssh 10.10.10.100 "who | wc -l") -gt 0 ] then;
+  active=1
+else
+  active=0
+fi
+sqlite3 $DIR/var/hbrain.db "UPDATE states SET active=$active WHERE name='HomeServer user'";
 
-torrents=$(/usr/bin# /usr/bin/ssh 10.10.10.100 "transmission-remote --list | sed '1d;$d' | grep -v Done | wc -l");
-sqlite3 $DIR/var/hbrain.db "UPDATE states SET active=$torrents WHERE name='HomeServer user'";
+if [ $(/usr/bin/ssh 10.10.10.100 "transmission-remote --list | sed '1d;$d' | grep -v Done | wc -l") -gt 0 ] then;
+  active=1
+else
+  active=0
+fi
+sqlite3 $DIR/var/hbrain.db "UPDATE states SET active=$active WHERE name='Torrenting'";
 
 # ako je server upaljen azuriraj waketime
 if [ $((serverlive)) -gt 0 ]; then
