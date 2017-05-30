479ccd076f3319a03988b6fabd01b8d45d82af8a
Marijo <mnovosel1@foi.hr>
2017-05-30 13:21:17 +0200

    statusNow.php update

--- statusNow.php
+++ statusNow.php
@@ -1,5 +1,8 @@
 #!/usr/bin/php
 <?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$path = dirname(__FILE__);
 
 $sqlitedb = new SQLite3($path .'/var/hbrain.db');
 $sqliteres = $sqlitedb->query("SELECT group_concat(active, '') FROM states ORDER BY rowid ASC");
