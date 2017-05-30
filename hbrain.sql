b348f3b4ee718db7b2d746e0e3d00962fbd49753
Marijo Novosel <marijo.novosel@gmail.com>
2017-05-30 11:37:55 +0200

    sql dumps

--- hbrain.sql
+++ hbrain.sql
@@ -3,23 +3,23 @@
 
         CREATE TABLE states (
             name varchar(75) NOT NULL,
-            auto int(1) NOT NULL DEFAULT 0,
+            auto int(1) NOT NULL DEFAULT 1,
             active int(1) NOT NULL DEFAULT 0
         );
 
-        INSERT INTO "states" VALUES('KODI',1,0);
+        INSERT INTO "states" VALUES('KODI',1,1);
         INSERT INTO "states" VALUES('HomeServer',1,0);
-        INSERT INTO "states" VALUES('HomeBrain user',0,0);
+        INSERT INTO "states" VALUES('HomeBrain user',0,1);
         INSERT INTO "states" VALUES('HomeServer user',0,0);
         INSERT INTO "states" VALUES('TV recording',0,0);
-        INSERT INTO "states" VALUES('Torrenting',0,0);
+        INSERT INTO "states" VALUES('Torrenting',0,1);
+        INSERT INTO "states" VALUES('MPD playing',1,1);
 
         CREATE TABLE changelog (
             timestamp DATETIME,
             statebefore varchar(30) NOT NULL,
             stateid int(11) NOT NULL,
             changedto int(1) NOT NULL,
-            weight int(11) NOT NULL DEFAULT 1,
             PRIMARY KEY(statebefore, stateid, changedto),
             FOREIGN KEY (stateid) REFERENCES states(rowid)
         );
@@ -29,22 +29,22 @@
             FOR EACH ROW
             WHEN OLD.active <> NEW.active
             BEGIN
-                INSERT OR REPLACE INTO changelog (timestamp, statebefore, stateid, changedto, weight)
+                INSERT OR REPLACE INTO changelog (timestamp, statebefore, stateid, changedto)
                 VALUES (
                             datetime('now','localtime'), 
                             (SELECT group_concat(active, '') FROM states ORDER BY rowid ASC), 
                             NEW.rowid, 
-                            NEW.active, 
-                            (SELECT weight+1 FROM changelog WHERE statebefore=(SELECT group_concat(active, '') FROM states ORDER BY rowid ASC) AND stateid=NEW.rowid AND changedto=NEW.active)
+                            NEW.active
                         );
-                DELETE FROM changelog WHERE timestamp <= date('now', '-30 day');
+                DELETE FROM changelog WHERE timestamp <= date('now', '-90 day');
             END;
 
 
         CREATE VIEW logic AS 
-            SELECT c.weight, c.statebefore, c.changedto, s.name
+            SELECT COUNT(*) AS weight, c.statebefore, c.changedto, s.name
                 FROM changelog c join states s ON c.stateid=s.rowid
-                ORDER BY c.weight desc, c.timestamp desc;
+                GROUP BY c.statebefore, c.stateid, c.changedto
+                ORDER BY weight desc, c.timestamp desc;
 
 
     COMMIT;
