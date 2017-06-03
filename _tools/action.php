57613fd777a69885a0148790578c49d7a0b03a9a
Marijo Novosel <marijo.novosel@gmail.com>
2017-06-03 17:36:42 +0200

    getStatus changed

--- _tools/action.php
+++ _tools/action.php
@@ -3,31 +3,11 @@
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 $path = dirname(__FILE__);
+$ret = array();
+$retBOOL = false;
 
 $sqlitedb = new SQLite3($path .'/var/hbrain.db');
 
-$sqliteres = $sqlitedb->query("SELECT group_concat(active, '') status FROM states ORDER BY rowid ASC");
-
-$status = $sqliteres->fetchArray(SQLITE3_ASSOC);
-$status = $status['status'];
-
-
-$sqliteres = $sqlitedb->query("SELECT rowid, * FROM states ORDER BY rowid ASC");
-while ($entry = $sqliteres->fetchArray(SQLITE3_ASSOC))
-{
-    $states[$entry['rowid']] = $entry['name'];
-}
-
-echo "Now I'm: |";
-for ($i=1; $i <= strlen($status); $i++)
-{ 
-    if ( $status[$i-1] == 1 )
-    {
-        echo $states[$i];
-        echo "|";
-    }
-}
-echo PHP_EOL;
 
 $sqliteres = $sqlitedb->query("SELECT COUNT(*) FROM logic 
                                 WHERE auto=1
@@ -38,7 +18,6 @@ $num = $sqliteres->fetchArray();
 
 if ( $num[0] > 0 )
 {
-    echo "should I: |";
     $sqliteres = $sqlitedb->query("SELECT name FROM logic
                                     WHERE auto=1
                                      AND hour=STRFTIME('%H', 'now')
@@ -46,9 +25,10 @@ if ( $num[0] > 0 )
                                 );
     while ($entry = $sqliteres->fetchArray(SQLITE3_ASSOC))
     {
-        echo $entry['name'];
-        echo "|";
+        $ret[] = $entry['name'];
+        $retBOOL = true;
     }
-    echo PHP_EOL;
 }
+
+if ( $retBOOL ) echo json_encode($ret);
 ?>
---
+++ _tools/status.php
@@ -0,0 +1,33 @@
+#!/usr/bin/php
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$path = dirname(__FILE__);
+$ret = array();
+$retBOOL = false;
+
+$sqlitedb = new SQLite3($path .'/var/hbrain.db');
+
+$sqliteres = $sqlitedb->query("SELECT group_concat(active, '') status FROM states ORDER BY rowid ASC");
+
+$status = $sqliteres->fetchArray(SQLITE3_ASSOC);
+$status = $status['status'];
+
+
+$sqliteres = $sqlitedb->query("SELECT rowid, * FROM states ORDER BY rowid ASC");
+while ($entry = $sqliteres->fetchArray(SQLITE3_ASSOC))
+{
+    $states[$entry['rowid']] = $entry['name'];
+}
+
+for ($i=1; $i <= strlen($status); $i++)
+{ 
+    if ( $status[$i-1] == 1 )
+    {
+        $ret[] = $states[$i];
+        $retBOOL = true;
+    }
+}
+
+if ( $retBOOL ) echo json_encode($ret);
+?>
