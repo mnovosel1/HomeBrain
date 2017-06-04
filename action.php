d01dfc82d87aa5cedae134be0a4bd0e5605c2906
Marijo Novosel <marijo.novosel@gmail.com>
2017-06-04 13:07:29 +0200

    action optimized

--- action.php
+++ action.php
@@ -8,20 +8,37 @@ $retBOOL = false;
 
 $sqlitedb = new SQLite3($path .'/var/hbrain.db');
 
+$sql = "
+                                 AND dow=STRFTIME('%w', 'now')*1
+                                 AND hour=STRFTIME('%H', 'now')*1 ";
+$sql2 = "
+                                 AND hour=STRFTIME('%H', 'now')*1 ";
+$sql_end = "
+                                 AND statebefore=(SELECT group_concat(active, '') FROM states ORDER BY rowid ASC)";
 
 $sqliteres = $sqlitedb->query("SELECT COUNT(*) FROM logic 
-                                WHERE auto=1
-                                 AND hour=STRFTIME('%H', 'now')
-                                 AND statebefore=(SELECT group_concat(active, '') FROM states ORDER BY rowid ASC)"
+                                WHERE auto=1 " . $sql . $sql_end
+                            );
+$num = $sqliteres->fetchArray();
+
+if ( $num[0] < 1 ) $sql = $sql2;
+
+$sqliteres = $sqlitedb->query("SELECT COUNT(*) FROM logic 
+                                WHERE auto=1 " . $sql . $sql_end
+                            );
+$num = $sqliteres->fetchArray();
+
+if ( $num[0] < 1 ) $sql = " ";
+
+$sqliteres = $sqlitedb->query("SELECT COUNT(*) FROM logic 
+                                WHERE auto=1 " . $sql . $sql_end
                             );
 $num = $sqliteres->fetchArray();
 
 if ( $num[0] > 0 )
 {
     $sqliteres = $sqlitedb->query("SELECT name FROM logic
-                                    WHERE auto=1
-                                     AND hour=STRFTIME('%H', 'now')
-                                     AND statebefore=(SELECT group_concat(active, '') FROM states ORDER BY rowid ASC)"
+                                WHERE auto=1 " . $sql . $sql_end
                                 );
     while ($entry = $sqliteres->fetchArray(SQLITE3_ASSOC))
     {
