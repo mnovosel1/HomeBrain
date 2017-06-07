58f870da7935b5769b0610fd9827a04fc2974511
Marijo Novosel <marijo.novosel@gmail.com>
2017-06-07 09:43:07 +0200

    HomeBrain.sh as api

--- HomeBrain.sh
+++ HomeBrain.sh
@@ -1,10 +1,69 @@
-#!/bin/bash
+#!/usr/bin/php
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-case "$1" in
+$path = dirname(__FILE__);
+$ret = array();
+$retBOOL = false;
 
-	"Testing")  echo "Sending TESTING signal"
-	   ;;
+$sqlitedb = new SQLite3($path .'/var/hbrain.db');
 
-	*) eval $1
-	   ;;
-esac
+
+// STATUS /////////////////////////////////////////////////////////////////////////////////////////////////////
+$sqliteres = $sqlitedb->query("SELECT group_concat(active, '') status FROM states ORDER BY rowid ASC");
+
+$status = $sqliteres->fetchArray(SQLITE3_ASSOC);
+$status = $status['status'];
+
+
+$sqliteres = $sqlitedb->query("SELECT rowid, * FROM states ORDER BY rowid ASC");
+while ($entry = $sqliteres->fetchArray(SQLITE3_ASSOC))
+{
+    $states[$entry['rowid']] = $entry['name'];
+}
+
+for ($i=1; $i <= strlen($status); $i++)
+{ 
+    $ret['status'][] = array($states[$i] => $status[$i-1]);
+}
+
+// ACTION /////////////////////////////////////////////////////////////////////////////////////////////////////
+$sql = "
+                                 AND dow=STRFTIME('%w', 'now')*1
+                                 AND hour=STRFTIME('%H', 'now')*1 ";
+$sql2 = "
+                                 AND hour=STRFTIME('%H', 'now')*1 ";
+$sql_end = "
+                                 AND statebefore=(SELECT group_concat(active, '') FROM states ORDER BY rowid ASC)";
+
+$sqliteres = $sqlitedb->query("SELECT COUNT(*) FROM logic 
+                                WHERE auto=1 " . $sql . $sql_end
+                            );
+$num = $sqliteres->fetchArray();
+
+if ( $num[0] < 1 ) $sql = $sql2;
+
+$sqliteres = $sqlitedb->query("SELECT COUNT(*) FROM logic 
+                                WHERE auto=1 " . $sql . $sql_end
+                            );
+$num = $sqliteres->fetchArray();
+
+if ( $num[0] < 1 ) $sql = " ";
+
+$sqliteres = $sqlitedb->query("SELECT COUNT(*) FROM logic 
+                                WHERE auto=1 " . $sql . $sql_end
+                            );
+$num = $sqliteres->fetchArray();
+
+if ( $num[0] > 0 )
+{
+    $sqliteres = $sqlitedb->query("SELECT name, changedto FROM logic
+                                WHERE auto=1 " . $sql . $sql_end
+                                );
+    while ($entry = $sqliteres->fetchArray(SQLITE3_ASSOC))
+    {
+        $ret['action'][] = array($entry['name'] => $entry['changedto']);
+    }
+}
+
+echo json_encode($ret);
