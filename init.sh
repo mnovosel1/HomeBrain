945ca506d93c995351bfd5491796e9d14dbab01c
Marijo Novosel <marijo.novosel@gmail.com>
2017-10-09 21:24:29 +0200

    tidying

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 if [ ! -f $DIR/var/hbrain.db ]; then
-  if [ -f $DIR/var_sav/hbrain.db ]; then
-    cp $DIR/var_sav/hbrain.db $DIR/var/hbrain.db
+  if [ -f $DIR/saved_var/hbrain.db ]; then
+    cp $DIR/saved_var/hbrain.db $DIR/var/hbrain.db
   else
     sqlite3 $DIR/var/hbrain.db -init $DIR/hbrain.sql &
   fi
